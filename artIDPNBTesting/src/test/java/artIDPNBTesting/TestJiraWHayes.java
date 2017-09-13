37bbfd49bc887aea70e26c74f5794bda8f47efe0
Wanda Hayes <nielsen.wh@hotmail.com>
2017-09-13 19:27:15 -0400

    Add test case for google seaerch.

---
+++ artIDPNBTesting/src/test/java/artIDPNBTesting/TestJiraWHayes.java
@@ -0,0 +1,21 @@
+package artIDPNBTesting;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class TestJiraWHayes {
+	@Test
+	public void TCWhayes()
+	{
+		System.out.println("TestCase for Google Search");
+		System.setProperty("webdriver.chrome.driver", "c:\\users\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+		driver.findElementByName("q").sendKeys("Zephyr for Jira");
+		driver.quit();
+
+	}
+}
