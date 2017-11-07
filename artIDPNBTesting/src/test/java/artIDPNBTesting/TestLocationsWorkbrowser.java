4fa84919763794f76e7029a32af1a3ac6a24858f
Wanda Hayes <wanda.hayes@nielsen.com>
2017-11-07 17:07:58 -0500

    Commit.  Add Locations mock testcase

--- artIDPNBTesting/src/test/java/artIDPNBTesting/TestLocationsWorkbrowser.java
+++ artIDPNBTesting/src/test/java/artIDPNBTesting/TestLocationsWorkbrowser.java
@@ -4,22 +4,21 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TestUdemy {
+public class TestLocationsWorkbrowser {
 
 	@Test 
 	public void testUdemyTestCase()
 	{
 		
-		System.out.println("TestCase Mock Locations Workbrowser test");
+		System.out.println("TestCase for Udemy Search modifying it");
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
 		
-			driver.get("https://ng2.answersuat.nielsen.com");
-			/*
+			//driver.get("http://www.udemy.com");
+			driver.get("http://www.");
 			driver.findElement(By.id("q")).clear();
 		    driver.findElement(By.id("q")).sendKeys("taxes");
 		    driver.findElement(By.xpath("//button[@type='submit']")).click();
-		    */
 		    driver.quit();
 		}
 	
