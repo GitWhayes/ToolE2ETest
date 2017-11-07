d0cba6f2a02c949eb74455dac85d566bc13f2b7e
Wanda Hayes <wanda.hayes@nielsen.com>
2017-11-07 17:03:00 -0500

    fail udemy test

--- artIDPNBTesting/src/test/java/artIDPNBTesting/TestUdemy.java
+++ artIDPNBTesting/src/test/java/artIDPNBTesting/TestUdemy.java
@@ -14,8 +14,9 @@ public class TestUdemy {
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
 		
-			driver.get("http://www.udemy.com");
-		    driver.findElement(By.id("q")).clear();
+			//driver.get("http://www.udemy.com");
+			driver.get("http://www.");
+			driver.findElement(By.id("q")).clear();
 		    driver.findElement(By.id("q")).sendKeys("taxes");
 		    driver.findElement(By.xpath("//button[@type='submit']")).click();
 		    driver.quit();
