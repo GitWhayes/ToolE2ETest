88675385135e64fae6ec89b69153a85814c3efab
Wanda Hayes <wanda.hayes@nielsen.com>
2017-11-14 12:18:47 -0500

    passing Locations test

--- artIDPNBTesting/src/test/java/artIDPNBTesting/TestLocationsWorkbrowser.java
+++ artIDPNBTesting/src/test/java/artIDPNBTesting/TestLocationsWorkbrowser.java
@@ -14,8 +14,8 @@ public class TestLocationsWorkbrowser {
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
 		
-			//driver.get("http://www.udemy.com");
-			driver.get("http://www.");
+			driver.get("http://www.udemy.com");
+			//driver.get("http://www.");
 			driver.findElement(By.id("q")).clear();
 		    driver.findElement(By.id("q")).sendKeys("taxes");
 		    driver.findElement(By.xpath("//button[@type='submit']")).click();
