2c19e4834e194f1cccb2d23b7cfff02577a802bd
Wanda Hayes <wanda.hayes@nielsen.com>
2017-11-14 12:17:13 -0500

    Updating TestUdemy.java to go to Nielsen.com

--- artIDPNBTesting/src/test/java/artIDPNBTesting/TestNielsen.java
+++ artIDPNBTesting/src/test/java/artIDPNBTesting/TestNielsen.java
@@ -4,17 +4,17 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TestUdemy {
+public class TestNielsen {
 
 	@Test 
 	public void testUdemyTestCase()
 	{
 		
-		System.out.println("\nTestCase Udemy Browser.");
+		System.out.println("TestCase Nielsen Browser.");
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
 		
-			driver.get("https://ng2.answersuat.nielsen.com");
+			driver.get("http://www.nielsen.com/us/en.html");
 			/*
 			driver.findElement(By.id("q")).clear();
 		    driver.findElement(By.id("q")).sendKeys("taxes");
